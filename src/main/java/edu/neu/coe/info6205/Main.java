ab2f2b64a2fbc1b6b9a474f17d6b294f24fe6e70
Altaf Shaikh <shaikh.alt@northeastern.edu>
2023-04-13 18:04:58 -0400

    main methods updated, christofides called

--- src/main/java/edu/neu/coe/info6205/Main.java
+++ src/main/java/edu/neu/coe/info6205/Main.java
@@ -1,8 +1,12 @@
 package edu.neu.coe.info6205;
 
 
+import edu.neu.coe.info6205.christofides.ChristofidesAlgorithm;
 import edu.neu.coe.info6205.model.Vertex;
+import edu.neu.coe.info6205.util.Benchmark;
 import edu.neu.coe.info6205.util.CSVReader;
+import edu.neu.coe.info6205.util.ChristofidesTour;
+
 import java.util.List;
 
 
@@ -11,5 +15,13 @@ public class Main {
     public static void main(String[] args) {
 
         List<Vertex> vertices = CSVReader.getVertices();
+        Benchmark benchmark = new Benchmark();
+        benchmark.startMark();
+        List<Vertex> tsp = ChristofidesAlgorithm.christofides(vertices);
+        benchmark.endMark();
+
+        System.out.print("\n Christofides Algorithm Time: " + benchmark.resultTime() + "ms");
+        System.out.println("\n Hamiltonian Path: " + ChristofidesTour.getTour(tsp));
+        System.out.println(" Christofides cost: " + ChristofidesTour.getTourCost(tsp) + " meters");
     }
 }
